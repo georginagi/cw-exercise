04d9aa58ba43c3ca997852c4736e7b84195ea4af
Georgina Giannoukou <g.giannoukou@gmail.com>
2018-06-25 00:31:48 +0200

    Developer class

---
+++ developer.py
@@ -0,0 +1,16 @@
+class Developers:
+    
+    def __init__(self, name, age, oss_projects):
+        self._name = name
+        self._age = age
+        self._oss_projects = oss_projects
+    
+    def name(self):
+        return self._name 
+    
+    def age(self):
+        return self._age
+    
+    def oss_projects(self):
+        return self._oss_projects
+
---
+++ order_by.py
@@ -0,0 +1,12 @@
+from operator import itemgetter, attrgetter
+
+developers = [
+    Developer
+]
+
+John, Age: 29, OSS Projects: 3
+Linda, Age: 29, OSS Projects: 5
+Robert, Age: 24, OSS Projects: 1
+Amanda, Age: 21, OSS Projects: 8
+Lawrence, Age: 32, OSS Projects: 2
+Steven, Age: 24, OSS Projects: 4
