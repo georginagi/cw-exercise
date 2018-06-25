6efe401d3ca1757075f908308f12622b94d79150
georgina <g.giannoukou@gmail.com>
2018-06-25 18:03:22 +0200

    playing around

--- developer.py
+++ developer.py
@@ -1,4 +1,4 @@
-from operator import itemgetter, attrgetter
+from operator import itemgetter, methodcaller
 
 class Developer:
     
@@ -16,13 +16,21 @@ class Developer:
     def oss_projects(self):
         return self._oss_projects
 
-    def sort_by(self, args):
-        pass
-
-
-devs = []
-devs.append(Developer("Sofia", 27, 5))
-devs.append(Developer("Anna", 27, 4))
-devs.sort(key=lambda x: x._oss_projects, reverse=True)
-for dev in devs:
+    def sort_by(self, args, reverse):
+        if len(args)==0:
+            print("invalid")
+        elif len(args)==1:
+            devs.sort(key=lambda x: x._oss_projects, reverse=True)
+        else:
+            devs.sort(key=lambda x, y: (x._name - x._oss_projects), reverse=False)
+devs = [
+    Developer("John", 29, 3),
+    Developer("Linda", 29, 5),
+    Developer("Robert", 24, 1),
+    Developer("Amanda", 21, 8),
+    Developer("Lawrence", 32, 2),
+    Developer("Steven", 24, 4)
+    ]
+#devs.sort(key=lambda x: (x._age + x._oss_projects), reverse=False)
+for dev in sorted(devs, key=methodcaller('name')):
     print("Name: {}, Age: {}, OSS_Projects: {}".format(dev.name(), dev.age(), dev.oss_projects()))
